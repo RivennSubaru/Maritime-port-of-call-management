ea63871e27b4bbe5b2015d962f223da5b5902dee
Rivenn <zafimenasteven@gmail.com>
2024-08-26 23:36:43 +0300

    Added incription component to 1st slide UserManager

--- src/pages/UserManager.jsx
+++ src/pages/UserManager.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
+import Inscription from './Inscription';
 
 function CustomTabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -45,19 +46,15 @@ const UserManager = () => {
     <Box sx={{ width: '100%' }}>
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
         <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-          <Tab label="Item One" {...a11yProps(0)} />
-          <Tab label="Item Two" {...a11yProps(1)} />
-          <Tab label="Item Three" {...a11yProps(2)} />
+          <Tab label="Inscrire utilisateur" {...a11yProps(0)} />
+          <Tab label="Liste des utilisateurs" {...a11yProps(1)} />
         </Tabs>
       </Box>
       <CustomTabPanel value={value} index={0}>
-        Item One
+        <Inscription/>
       </CustomTabPanel>
       <CustomTabPanel value={value} index={1}>
         Item Two
-      </CustomTabPanel>
-      <CustomTabPanel value={value} index={2}>
-        Item Three
       </CustomTabPanel>
     </Box>
   );
